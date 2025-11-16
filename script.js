26cdb95fe09c4dd6d4ae17420766d69634598472
Aloba busayo <alobabusayo01@gmail.com>
2025-11-16 22:12:18 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+
+<div>
+    <input id="showRandomQuoteText" type="text" placeholder="show a new quote" />
+    <input id="showRandomQuoteCategory" type="text" placeholder="Enter quote category" />
+    <button onclick="showQuote()">show Quote</button>
+  </div>
+<div>
+    <input id="newQuoteText" type="text" placeholder="Enter a new quote" />
+    <input id="newQuoteCategory" type="text" placeholder="Enter quote category" />
+    <button onclick="addQuote()">Add Quote</button>
+  </div>
