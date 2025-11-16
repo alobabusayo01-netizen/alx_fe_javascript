df78cbd582e72736c6eeaa69775983fdffccdf32
Aloba busayo <alobabusayo01@gmail.com>
2025-11-16 22:12:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
     <input id="showRandomQuoteCategory" type="text" placeholder="Enter quote category" />
     <button onclick="showQuote()">show Quote</button>
   </div>
+    
 <div>
     <input id="newQuoteText" type="text" placeholder="Enter a new quote" />
     <input id="newQuoteCategory" type="text" placeholder="Enter quote category" />
